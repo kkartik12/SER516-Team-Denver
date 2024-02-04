1f26ed7c052adbd2846e3f832a0128e8f22719b7
Kushagra1480 <kkartik@asu.edu>
2024-02-03 18:46:41 -0700

    Task #59 (Add Controller Class) Done

---
+++ src/main/java/Controller.java
@@ -0,0 +1,23 @@
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RestController
+@RequestMapping("/api") // Base path for API endpoints
+public class Controller {
+
+    @PostMapping("/login")
+    @ResponseBody
+    public String login(@RequestParam String username, @RequestParam String passoword) {
+        String authToken = Authentication.authenticate(username, passoword);
+        if(authToken != null) {
+            return authToken;
+        } else {
+            return "Invalid Credentials";
+        }
+    }
+
+}
