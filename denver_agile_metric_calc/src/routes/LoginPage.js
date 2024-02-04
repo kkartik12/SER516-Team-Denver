c6c348f7ecc23d4b617c694b7b4ac16f187a3876
Kushagra1480 <kkartik@asu.edu>
2024-02-04 15:14:38 -0700

    Added Styles to Login Page

--- denver_agile_metric_calc/src/routes/LoginPage.js
+++ denver_agile_metric_calc/src/routes/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { redirect } from 'react-router-dom';
-
+import '../styles/LoginPage.css'
 
 function AuthenticationForm() {
   const [username, setUsername] = useState('')
@@ -27,26 +27,37 @@ function AuthenticationForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Username:</label>
-      <input
-        type="text"
-        id="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <br />
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        id="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <br />
-      {errorMessage && <p className="error">{errorMessage}</p>}
-      <button type="submit">Log in</button>
-    </form>
+    <div className='login-container'>
+      
+      <div className='image-container'>
+      <h1>Agile Metrics</h1>
+        <img
+        src="https://facts.net/wp-content/uploads/2023/09/21-facts-about-lightning-mcqueen-cars-1694564602.jpg" // Replace with the actual image path
+        alt="Lightning McQueen"
+        className="profile-picture"
+        />
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder='Username'
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder='Password'
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        {errorMessage && <p className="error">{errorMessage}</p>}
+        <button type="submit">LOG IN</button>
+      </form>
+    </div>
   );
 }
 
