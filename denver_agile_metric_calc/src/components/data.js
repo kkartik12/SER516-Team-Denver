8ace8373df4b2317c00ab560021124f803610f16
vrupak <143535096+vrupak@users.noreply.github.com>
2024-02-04 18:17:11 -0700

    Added project creation date in project display page.

--- denver_agile_metric_calc/src/components/data.js
+++ denver_agile_metric_calc/src/components/data.js
@@ -2,26 +2,31 @@ export const projects = [{
     id: 0,
     name: 'Project 1',
     description: 'This is the description for project 1.',
+    created_date: "2020-07-02T11:56:21.140022+00:00",
     imageId: 'MK3eW3A'
   }, {
     id: 1,
     name: 'Project 2',
     description: 'This is the description for project 2.',
+    created_date: "2021-07-06T11:56:21.14223+00:00",
     imageId: 'mynHUSa'
   }, {
     id: 2,
     name: 'Project 3',
     description: 'This is the description for project 3.',
+    created_date: "2022-07-01T11:56:21.14364+00:00",
     imageId: 'bE7W1ji'
   }, {
     id: 3,
     name: 'Project 4',
     description: 'This is the description for project 4.',
+    created_date: "2023-07-03T11:56:21.144998+00:00",
     imageId: 'IOjWm71'
   }, {
     id: 4,
     name: 'Project 5',
     description: 'This is the description for project 5.',
+    created_date: "2019-07-04T11:56:21.144998+00:00",
     imageId: 'lrWQx8l'
   }];
   
