2ee677d7a18d5fe1aa0ea5e2da97a3dc616bdb99
vrupak <143535096+vrupak@users.noreply.github.com>
2024-02-04 17:09:20 -0700

    updated the look of the diplay project page.

--- denver_agile_metric_calc/src/App.js
+++ denver_agile_metric_calc/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './routes/LoginPage'
+import LoginPage from './routes/LoginPage';
 
 
 function App() {
   return (
     <div className='container'>
-        <LoginPage />
+          <LoginPage />
     </div>
-
   );
 }
 
