71befb2481475d73d61f65b208a9cc51855101cb
adate1 <adate1@asu.edu>
2024-04-05 14:19:52 -0700

    Task 209 done

---
+++ Microservices/found_work/src/main/java/found_work/found_work/FoundWorkApplication.java
@@ -0,0 +1,13 @@
+package found_work.found_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundWorkApplication.class, args);
+	}
+
+}
---
+++ Microservices/found_work/src/test/java/found_work/found_work/FoundWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package found_work.found_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
