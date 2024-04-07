22b571dcc36308fa4e0636436d3eecd3618413b0
Aarti Kalekar <aartikalekar01@gmail.com>
2024-04-07 12:08:38 -0700

    Task #234 (US 233) Create spring boot project for lead time

---
+++ Microservices/leadtime/src/test/java/com/example/leadtime/LeadtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leadtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
