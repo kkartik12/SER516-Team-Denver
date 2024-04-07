ca720779069ffb7f92dd127ee3ac5ccaf5691d5b
Aarti Kalekar <aartikalekar01@gmail.com>
2024-04-07 12:00:49 -0700

    Task #230 (US 229) Fixed dir structure

---
+++ Microservices/cycletime/src/main/java/com/example/cycletime/CycletimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cycletime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycletimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CycletimeApplication.class, args);
+	}
+
+}
